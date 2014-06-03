0b8b6c812a2fdefe2c6fe2d431b091f536f15bd0
sumayyah <sumayyah16.m@gmail.com>
2014-06-03 00:44:11 -0700

    added fragments

---
+++ obdEnergy/src/main/java/com/example/obdenergy/obdenergy/Activities/MetricFragment.java
@@ -0,0 +1,72 @@
+package com.example.tabactivity.app.Activity;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.tabactivity.app.Utilities.Console;
+import com.example.tabactivity.app.MainActivity;
+import com.example.tabactivity.app.R;
+
+/**
+ * Created by sumayyah on 5/31/14.
+ */
+public class MetricFragment extends Fragment {
+
+    private MainActivity mainActivity;
+    private TextView activityTitle;
+    private TextView fuelData;
+    private TextView carbonData;
+    private TextView treesData;
+
+    private String gallons = "0.0";
+    private String carbonUsed = "0.0";
+    private String treesKilled = "0.0";
+
+    private String classID = "MetricFragment ";
+    private String messageFromMain = "nothing";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstancestate){
+
+        Console.log(classID + " creation");
+
+        View view = inflater.inflate(R.layout.metrics_fragment, container, false);
+
+        activityTitle = (TextView)(view.findViewById(R.id.activityTitle));
+        fuelData = (TextView)(view.findViewById(R.id.fuelData));
+        carbonData = (TextView)(view.findViewById(R.id.carbonData));
+        treesData = (TextView)(view.findViewById(R.id.metricData));
+        activityTitle.setText(messageFromMain);
+
+        fuelData.setText(gallons);
+        carbonData.setText(carbonUsed);
+        treesData.setText(treesKilled);
+
+
+        //TODO: get miles and mpg and calculate data
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        this.mainActivity = (MainActivity) activity;
+    }
+
+    /*Data from Main is given before onCreateView - so it needs to be stored in a global for onCreateView to access later*/
+    public void MetricFragmentDataComm(String message){
+        Console.log(classID+" Main sent: "+message);
+        messageFromMain = message;
+    }
+    public void MetricFragmentDataComm(String gallons, String carbonUsed, String treesKilled){
+        this.gallons = gallons;
+        this.carbonUsed = carbonUsed;
+        this.treesKilled = treesKilled;
+    }
+}
