1680e4ff652ab099a0c6328055e27391195cb340
sumayyah <sumayyah16.m@gmail.com>
2014-07-16 11:31:33 -0700

    Added new tree images

--- obdEnergy/src/main/java/com/example/obdenergy/obdenergy/Utilities/GridAdapter.java
+++ obdEnergy/src/main/java/com/example/obdenergy/obdenergy/Utilities/GridAdapter.java
@@ -9,6 +9,8 @@ import android.widget.ImageView;
 
 import com.example.obdenergy.obdenergy.R;
 
+import java.util.ArrayList;
+
 /**
  * Created by Sumayyah on 6/11/2014.
  */
@@ -17,11 +19,15 @@ public class GridAdapter extends BaseAdapter {
     private Context context;
     private int imageNum;
     private String imageType;
+    private String[] treeList;
 
     public GridAdapter(Context context, int imageNum, String imageType){
         this.context = context;
         this.imageNum = imageNum;
         this.imageType = imageType;
+
+        treeList = new String[]{"R.drawable.tree1leaf", "R.drawable.tree2leaves", "R.drawable.tree3leaves", "R.drawable.tree4leaves", "R.drawable.tree5leaves", "R.drawable.tree6leaves", "R.drawable.tree7leaves", "R.drawable.tree8leaves", "R.drawable.tree9leaves", "R.drawable.tree10leaves"};
+        Console.log("New GridAdapter with treelist 1 and 3 "+treeList[0]+" "+treeList[1]);
     }
 
     @Override
@@ -39,47 +45,19 @@ public class GridAdapter extends BaseAdapter {
 
             if(imageType.equals("CARBON")){
                 imageView.setImageResource(R.drawable.cloud_icon);
-            }else if(imageType.equals("TREE")) imageView.setImageResource(R.drawable.leafcopy);
+            }else if(imageType.equals("TREE")) {
+                imageView.setImageResource(R.drawable.leafcopy);
+                setTreeImage();
+            }
 
         return gridView;
 
-//        if (convertView == null) convertView = inflater.inflate(R.layout.grid_contents, null);
-//
-//        //set image based on selected text
-//            ImageView imageView = (ImageView) convertView
-//                    .findViewById(R.id.gridImage);
-//
-//            if(imageType.equals("CARBON")){
-//                imageView.setImageResource(R.drawable.cloud_icon);
-//            }else if(imageType.equals("TREE")) imageView.setImageResource(R.drawable.leafcopy);
-//
-//        return convertView;
-//        View gridView;
-//
-//        if (convertView == null) {
-////            Console.log("Creating new grid view");
-//
-////            gridView = new View(context);
-//
-//            // get layout from mobile.xml
-//            gridView = inflater.inflate(R.layout.grid_contents, null);
-//
-//            // set image based on selected text
-//            ImageView imageView = (ImageView) gridView
-//                    .findViewById(R.id.gridImage);
-//
-//            if(imageType.equals("CARBON")){
-//                imageView.setImageResource(R.drawable.cloud_icon);
-//            }else if(imageType.equals("TREE")) imageView.setImageResource(R.drawable.leafcopy);
-//
-//
-//
-//
-//        } else {
-////            Console.log("using old grid view");
-//            gridView = (View) convertView;
-//        }
-//        return gridView;
+    }
+
+    private void setTreeImage(){
+        Console.log("Gridview set tree image with number "+imageNum);
+
+
     }
 
     @Override
